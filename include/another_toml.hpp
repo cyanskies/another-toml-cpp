046e03bc5c435ae33c6a6fead3fffb6df2894782
Steven Pilkington <cyanskies@users.noreply.github.com>
2022-10-05 19:27:51 +1100

    writing the actual parser

--- include/another_toml.hpp
+++ include/another_toml.hpp
@@ -5,49 +5,123 @@
 #include <istream>
 #include <memory>
 #include <string_view>
+#include <vector>
+
+// library for parsing toml1.0 documents
 
 namespace another_toml
 {
 	namespace detail
 	{
-		struct internal_node;
+		using index_t = std::size_t;
 		struct toml_internal_data;
 
 		struct no_throw_t {};
 	}
 
-	class toml_node 
+	enum class node_type
+	{
+		table,
+		array, 
+		key,
+		value,
+		comment // note, we don't provide access to comments or preserve them
+	};
+
+	class node;
+
+	class node_iterator
 	{
 	public:
-		toml_node(std::shared_ptr<detail::toml_internal_data> shared_data = {},
-			detail::internal_node* node = {})
-			: _data{ shared_data }, _node{ node } {}
+		using iterator_concept = std::forward_iterator_tag;
+	};
 
-		std::string_view name() const;
+	class node 
+	{
+	public:
+		node(std::shared_ptr<detail::toml_internal_data> shared_data = {},
+			detail::index_t i = {})
+			: _data{ shared_data }, _index{ i } {}
 
-		std::vector<toml_node> children();
-		//std::string_view full_name
+		//test if this is a valid node: calling any function other than good()
+		//								on an invalid node is undefined behaviour
+		// this will return false for nodes returned by the fucntions:
+		//	get_child() for a node that has no children
+		//	get_next() for a node that has no more siblings
+		//  find()/find_child() if the node wasn't found
+		//
+		// if you use iterator/ranges to access child nodes then you don't have to worry about this.
+		bool good() const noexcept;
 
+		// test node type
+		bool table() const noexcept;
+		bool array() const noexcept;
+		bool key() const noexcept;
+		bool value() const noexcept;
+
+		// get the nodes children
+		// children are any nodes deeper in the heirarchy than the current node
+		// eg. for a table: subtables, keys, arrays(of subtables)
+		//		for an array: arrays, values, tables
+		//		for a value: none
+		//		for a key: anonymous table, value, array(of arrays of values)
+		bool has_children() const noexcept;
+		std::size_t child_count() const noexcept;
+		node get_child() const noexcept;
+		std::vector<node> get_children() const;
+		node get_next_sibling() const noexcept;
+
+		// search all children for the named node
+		// can use dots to search for subchildren
+		node find_child(std::string_view) const noexcept;
+
+
+		// iterator based interface
+		node_iterator begin() const noexcept;
+		node_iterator end() const noexcept;
+		std::size_t size() const noexcept;
+
+		std::string_view as_string() const noexcept;
+		// as_XXX
+
+		//returns the root table at the base of the hierarchy
+		node get_root_table() const noexcept;
+		// works like find_child, but starts from the root
+		node find(std::string_view name) const noexcept;
+		
 		operator bool() const noexcept
 		{
-			return static_cast<bool>(_data);
+			return good();
 		}
 
 	private:
 		mutable std::shared_ptr<detail::toml_internal_data> _data;
-		detail::internal_node* _node = {};
+		detail::index_t _index;
 	};
 
 	constexpr auto no_throw = detail::no_throw_t{};
 
-	toml_node parse(std::string_view toml);
-	toml_node parse(std::istream&); 
-	toml_node parse(const std::filesystem::path& filename);
+	node parse(std::string_view toml);
+	node parse(std::istream&); 
+	node parse(const std::filesystem::path& filename);
 
-	toml_node parse(std::string_view toml, detail::no_throw_t) noexcept;
-	toml_node parse(std::istream&, detail::no_throw_t) noexcept;
-	toml_node parse(const std::filesystem::path& filename, detail::no_throw_t) noexcept;
+	node parse(std::string_view toml, detail::no_throw_t) noexcept;
+	node parse(std::istream&, detail::no_throw_t) noexcept;
+	node parse(const std::filesystem::path& filename, detail::no_throw_t) noexcept;
 	
+}
+
+namespace std
+{
+	template<>
+	struct iterator_traits<another_toml::node_iterator>
+	{
+		using difference_type = std::ptrdiff_t;
+		using value_type = another_toml::node;
+		using pointer = value_type*;
+		using reference = const value_type&;
+		using iterator_category = std::forward_iterator_tag;
+	};
 }
 
 #include "another_toml.inl"
--- source/another_toml.cpp
+++ source/another_toml.cpp
@@ -1,65 +1,258 @@
 #include "another_toml.hpp"
 
+#include <array>
 #include <cassert>
 #include <fstream>
+#include <iostream>
 #include <ranges>
 #include <sstream>
+#include <variant>
 
 namespace another_toml
 {
 	namespace detail
-	{
+	{	
 		struct internal_node
 		{
-			// line that the table header starts at, 
-			// also used as unique keys for tables
-			std::streampos loc = {};
-			// full name is name_prefix + name
+			// includes the table names
 			std::string name;
-			//std::string name_prefix;
-			std::vector<internal_node> child_nodes;
+			node_type type;
+			index_t next;
+			index_t child;
+			bool closed = false; // a closed table can still have child tables added, but not child keys
 		};
+
+		const auto bad_index = std::numeric_limits<index_t>::max();
 
 		struct toml_internal_data
 		{
+			static constexpr auto root_table = index_t{};
 			std::vector<internal_node> tables;
 		};
 	}
 
+	using detail::index_t;
+
+	index_t find_table(std::string_view name) noexcept;
+	void insert_child(detail::toml_internal_data&, index_t parent, detail::internal_node n);
+
+	enum class mode
+	{
+		key_name,
+		value,
+		root,
+	};
+
+	enum class token_type
+	{
+		table_begin,
+		table_end,
+		array_begin,
+		array_end,
+		key_value,
+		value,
+		comment,
+		bad
+	};
+
+	struct token
+	{
+		index_t parent;
+		token_type type;
+		std::optional<std::string> value;
+	};
+
+	struct parser_state
+	{
+		std::istream& strm;
+		token prev;
+		mode mode = mode::root;
+		std::vector<node_type> stack = { node_type::table };
+		index_t parent = detail::bad_index;
+		index_t prev_sibling = detail::bad_index;
+		std::size_t line = {};
+		std::size_t col = {};
+	};
+
+	//Why is '\n' a forbidden char? That makes it impossible to end a comment
+	constexpr auto comment_forbidden_chars = std::array<char, 30>{
+		//U+0000 to U+0008
+		'\0', 1, 2, 3, 4, 5, 6, '\a', '\b',
+		//U+000A to U+001F
+		/*'\n',*/ '\v', '\f', /*'\r',*/ 14, 15, 16, 17, 18, 19, 20,
+		21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+		//U+007F
+		127
+	};
+
+	constexpr auto comment_begin = '#';
+	
+	constexpr bool valid_utf8_char(char) noexcept; //??? how to implement
+
+	// limit to a-z, A-Z, 0-9, '-' and ' '
+	constexpr bool valid_key_name_char(char ch) noexcept
+	{
+		if (ch == '-')
+			return true;
+
+		if (ch >= '0' && ch <= '9')
+			return true;
+
+		if (ch >= 'A' && ch <= 'Z')
+			return true;
+
+		if (ch == '_')
+			return true;
+
+		if (ch >= 'a' && ch <= 'z')
+			return true;
+
+		return false;
+	}
+
+	static std::optional<std::string> parse_key_name(parser_state& strm, char start)
+	{
+
+	}
+
 	template<bool NoThrow>
-	static toml_node make_base_node(std::istream& strm) noexcept(NoThrow)
+	static node parse_array() noexcept(NoThrow)
+	{
+	}
+
+	static bool whitespace(char ch) noexcept
+	{
+		return ch == ' ' || ch == '\t';
+	}
+
+	//new line(we check both, since file may have been opened in binary mode)
+	static bool newline(parser_state& strm, char ch) noexcept
+	{
+		if (ch == '\r' && strm.strm.peek() == '\n')
+		{
+			strm.strm.ignore();
+			return true;
+		}
+
+		return ch == '\n';
+	}
+
+	static void parse_comment(parser_state& strm) noexcept
+	{
+		while (strm.strm.good())
+		{
+			auto ch = strm.strm.get();
+			if (newline(strm, ch))
+			{
+				strm.col = {};
+				++strm.line;
+				return;
+			}
+
+			if (std::ranges::any_of(comment_forbidden_chars, [ch](auto& badch) {
+				return ch == badch;
+				}))
+			{
+				std::cerr << "Forbidden character in comment: " << ch << '(' << static_cast<int>(ch) << ")\n";
+			}
+
+			++strm.col;
+		}
+	}
+
+	template<bool NoThrow>
+	static token parse_token(parser_state& strm, detail::toml_internal_data&) noexcept(NoThrow)
+	{
+		// parser candidates
+		// table
+		// array of table
+		// key
+		// comment
+		while (strm.strm.good())
+		{
+			auto ch = strm.strm.get();
+			++strm.col;
+
+			auto equal_to_ch = [ch](auto&& other) noexcept {
+				return ch == other;
+			};
+
+			// whitespace
+			if (whitespace(ch))
+			{
+				continue;
+			}
+
+			if(newline(strm, ch))
+			{
+				strm.col = {};
+				++strm.line;
+				continue;
+			}
+
+			if (ch == '[') // start table
+			{
+				if (strm.strm.peek() == '[')//array of tables
+				{
+					;//parse_key_name(strm);
+				}
+				else
+					;//parse_key_name(strm);
+
+					continue;
+			}
+
+			if (ch == '#')
+			{
+				parse_comment(strm);
+			}
+
+			//auto key_str = parse_key_name(strm);
+		}
+
+		return {};
+	}
+
+	template<bool NoThrow>
+	static node parse_toml(std::istream& strm) noexcept(NoThrow)
 	{
 		auto toml_data = std::make_shared<detail::toml_internal_data>();
 		auto& t = toml_data->tables;
 
+		// implicit global table
+		// always stored at index 0
+		t.emplace_back(std::string{}, node_type::table);
+		auto p_state = parser_state{ strm };
 
+		auto current_index = index_t{};
 
+		while(!strm.eof())
+		{
+			auto token = parse_token<NoThrow>(p_state, *toml_data);
 
+		}
 
-		auto root_map = std::ranges::find(t, std::streampos{}, &detail::internal_node::loc);
-		assert(root_map != end(t));
-
-		return { std::move(toml_data), &*root_map };
+		return { std::move(toml_data), {} };
 	}
 
 	template<bool NoThrow>
-	toml_node parse(std::string_view toml) noexcept(NoThrow)
+	node parse(std::istream& strm) noexcept(NoThrow)
+	{
+		if (!strm.good())
+			return {};
+
+		return parse_toml<NoThrow>(strm);
+	}
+
+	template<bool NoThrow>
+	node parse(std::string_view toml) noexcept(NoThrow)
 	{
 		auto strstream = std::stringstream{ std::string{ toml }, std::ios_base::in };
 		return parse<NoThrow>(strstream);
 	}
 
 	template<bool NoThrow>
-	toml_node parse(std::istream& strm) noexcept(NoThrow)
-	{
-		if (!strm.good())
-			return {};
-
-		return make_base_node<NoThrow>(strm);
-	}
-
-	template<bool NoThrow>
-	toml_node parse(const std::filesystem::path& path) noexcept(NoThrow)
+	node parse(const std::filesystem::path& path) noexcept(NoThrow)
 	{
 		if (!std::filesystem::exists(path))
 			return {};
@@ -71,32 +264,32 @@ namespace another_toml
 		return parse<NoThrow>(strm);
 	}
 
-	toml_node parse(std::string_view toml)
+	node parse(std::string_view toml)
 	{
 		return parse<false>(toml);
 	}
 
-	toml_node parse(std::istream& strm)
+	node parse(std::istream& strm)
 	{
 		return parse<false>(strm);
 	}
 
-	toml_node parse(const std::filesystem::path& path)
+	node parse(const std::filesystem::path& path)
 	{
 		return parse<false>(path);
 	}
 
-	toml_node parse(std::string_view toml, detail::no_throw_t) noexcept
+	node parse(std::string_view toml, detail::no_throw_t) noexcept
 	{
 		return parse<true>(toml);
 	}
 
-	toml_node parse(std::istream& strm, detail::no_throw_t) noexcept
+	node parse(std::istream& strm, detail::no_throw_t) noexcept
 	{
 		return parse<true>(strm);
 	}
 
-	toml_node parse(const std::filesystem::path& filename, detail::no_throw_t) noexcept
+	node parse(const std::filesystem::path& filename, detail::no_throw_t) noexcept
 	{
 		return parse<true>(filename);
 	}
